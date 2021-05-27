5b31bb418037709973c6813ed1120931c7d0b3f9
SoullessSoldier <gevork_ssh@mail.ru>
2021-05-27 19:17:24 +0200

    Lesson01

--- lesson01/js/script.js
+++ lesson01/js/script.js
@@ -1,1 +1,10 @@
-/* */
+let money = 100;
+let income = 1;
+let addExpenses = true;
+let deposit = 0.00;
+let mission = 1;
+let period = 'year';
+
+alert('Here will be your ad');
+
+console.log('Console says:', 'mooo');
