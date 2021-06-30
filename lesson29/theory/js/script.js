3be745ea909d2bda66738a10c0e65091bcacfbd8
Soulless Soldier <gevork_ssh@mail.ru>
2021-06-30 14:16:27 +0200

    add Lesson29 theory

--- lesson29/theory/js/script.js
+++ lesson29/theory/js/script.js
@@ -1,1 +1,31 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', ()=>{
+    'use strict';
+    const select = document.getElementById('cars');
+    const output = document.getElementById('output');
+    select.addEventListener('change', ()=>{
+        const request = new XMLHttpRequest();
+        request.open('GET', './cars.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+        request.addEventListener('readystatechange', event => {
+            if(request.readyState === 4 && request.status === 200){
+                
+                const data = JSON.parse(request.responseText);
+               
+                data.cars.forEach(item => {
+                    if(item.brand === select.value) {
+                        
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Машина ${brand} ${model}<br>Цена ${price}`;
+                    }
+                });
+
+
+            } else {
+                output.innerHTML = `Произошла ошибка`;
+            }
+            
+        });
+    });
+
+});
