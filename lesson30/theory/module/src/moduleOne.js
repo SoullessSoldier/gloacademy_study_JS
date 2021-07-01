e8feecf6bc0a428cf902aba257ecd3e2e2bef7aa
SoullessSoldier <gevork_ssh@mail.ru>
2021-07-01 21:27:02 +0200

    add Lesson30 theory

--- lesson30/theory/module/src/moduleOne.js
+++ lesson30/theory/module/src/moduleOne.js
@@ -1,7 +1,11 @@
 const moduleTwo = require("./moduleTwo");
-
+/*
 function moduleOne(){
-    console.log('hello');
+    console.log('hello!!!???');
 }
 
 module.exports = moduleOne;
+*/
+let a = 3;
+let b = 5;
+export {a as one, b as two}
--- lesson30/theory/module/src/moduleTwo.js
+++ lesson30/theory/module/src/moduleTwo.js
@@ -1,3 +1,4 @@
+/*
 const moduleTwo = () => {
     setInterval(()=>{
         document.body.innerHTML = Date();
@@ -5,3 +6,10 @@ const moduleTwo = () => {
 };
 
 module.exports = moduleTwo;
+*/
+export default function moduleTwo(a, b) {
+    setInterval(()=>{
+        document.body.innerHTML = Date();
+    }, 1000);
+    return a+b;
+};
