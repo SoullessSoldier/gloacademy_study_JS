bcde1bc9b5db053e2e3683e96df6743fe6b1dd94
SoullessSoldier <gevork_ssh@mail.ru>
2021-07-12 22:27:23 +0200

    step2

--- diploma-basic/src/index.js
+++ diploma-basic/src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import smoothScrollToAnchor from './modules/smoothScrollToAnchor';
+import toggleModalCallback from './modules/toggleModalCallback';
 
 smoothScrollToAnchor();
+toggleModalCallback();
--- diploma-basic/src/modules/smoothScrollToAnchor.js
+++ diploma-basic/src/modules/smoothScrollToAnchor.js
@@ -1,17 +1,5 @@
 const smoothScrollToAnchor = () => {
-    /*    
-    const elementScrollFrom = document.querySelector('[href="#service-block"]');
-    let atrribute = elementScrollFrom.getAttribute('href').slice(1,);
-
-    let elementScrollTo = document.getElementById(`${atrribute}`);
     
-    elementScrollFrom.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: elementScrollTo.offsetTop,
-            behavior: "smooth"
-        });
-    }); */
     const anchorLinkService = document.querySelector('[href="#services"]');
     const anchorLinkFaq = document.querySelector('[href="#faq"]');
     const anchorLinkContacts = document.querySelector('[href="#contacts"]');
